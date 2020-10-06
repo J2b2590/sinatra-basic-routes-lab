b1bd0f4a2f89f19c543edb764a78f6bfc86a5c7f
J2b2590 <J2b2590@gmail.com>
2020-10-06 10:35:48 -0500

    finished lab

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+      end
+      get '/name' do
+        "My name is Jef"
+      end
+      get '/hometown' do
+        "My hometown is Savannah"
+      end
+      get '/favorite-song' do
+        "My favorite song is No Quarter"
+      end
 end
